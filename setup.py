b1395b2cf59855e124621ecdc66a3855a57c0ef5
Kiarash Hosseini <k.hosseini1130@gmail.com>
2022-04-20 16:08:23 -0700

    Upload datasets

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     package_dir={"": "src"},
+    package_data={'gender-gap-in-iran': ["data/*.zip"]},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3",
 )
---
+++ tests/test_map.py
@@ -0,0 +1,9 @@
+from gender_gaps import maps
+
+
+def test_answer():
+    assert maps.temp(3) == 3
+
+
+def another_test():
+    assert maps.temp(5) ==5
